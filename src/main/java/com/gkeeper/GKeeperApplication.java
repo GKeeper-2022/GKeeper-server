f8c4c5a9bfa38c9a5c0991c929a2ad75c36cb803
DESKTOP-EOPTQ2E\duwls <t781797@gmail.com>
2022-01-25 15:53:13 +0900

    project 생성

---
+++ src/main/java/com/gkeeper/GKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.gkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gkeeper/GKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
