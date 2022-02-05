247a783654d4ea5b788f3c2300ecad16d9b22a53
DESKTOP-EOPTQ2E\duwls <t781797@gmail.com>
2022-02-05 15:45:10 +0900

    user table 생성

---
+++ src/main/java/com/gkeeper/entity/User.java
@@ -0,0 +1,44 @@
+package com.gkeeper.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+
+public class User {
+
+	@Id
+	@Column(name="userId")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer userId;
+	
+	@Column(name="userEmail", unique=true)
+	private String userEmail;
+	
+	
+	@Column(name="userDate", nullable=false, updatable=false, insertable=false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private Timestamp userDate;
+	
+	@Column(name="userActiveDate")
+	private Timestamp userActiveDate;
+
+	
+	@Column(name="activated", columnDefinition="BOOLEAN DEFAULT 1")
+	private boolean activated;
+}
