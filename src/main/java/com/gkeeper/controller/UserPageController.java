af83e50d94d9a2749aa040af3832d0900918311c
DESKTOP-EOPTQ2E\duwls <t781797@gmail.com>
2022-03-04 16:14:51 +0900

    UserPageController 수정

--- src/main/java/com/gkeeper/controller/UserPageController.java
+++ src/main/java/com/gkeeper/controller/UserPageController.java
@@ -2,23 +2,32 @@ package com.gkeeper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.gkeeper.service.UserService;
-
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/user")
 public class UserPageController {
 	
+	@GetMapping("")
+	@ApiOperation(value = "회원별 페이지", notes ="회원 마이페이지")
+	public String openUserPage() {
+		return "userPage";
+	}
+	
 	@GetMapping("/login")
+	@ApiOperation(value = "로그인")
 	public String openLoginPage() {
-		return "user/login";
+		return "login";
 	}
 	
 	@GetMapping("/signup")
+	@ApiOperation(value = "회원가입")
 	public String openSingUpage() {
-		return "user/signup";
+		return "signup";
 	}
 
 }
