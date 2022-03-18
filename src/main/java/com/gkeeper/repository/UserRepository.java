12ec04431bc1fcc7beb8ba58dc38d1003ecdc03d
spacekay <bethjkim0311@gmail.com>
2022-03-18 12:49:42 +0900

    일부 import문 삭제

--- src/main/java/com/gkeeper/repository/UserRepository.java
+++ src/main/java/com/gkeeper/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.gkeeper.repository;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
